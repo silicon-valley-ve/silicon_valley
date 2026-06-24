4c892cdd206fb94035261bbefb909808d33366d9
dsojo1980 <dsojo.tanfe@gmail.com>
2026-06-24 14:53:51 -0400

    modulo extension nota de debito

--- Localizacion_contable_v19/iva_retention/__manifest__.py
+++ Localizacion_contable_v19/iva_retention/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 {
-        'name': 'Retenciones de IVA para Venezuela v18',
+        'name': 'Retenciones de IVA para Venezuela v19',
         'version': '0.1',
-        'author': 'Ing. Darrell Sojo / Alianza Frank Service',
-        'summary': 'Retenciones de IVA para Venezuela v18',
-        'description': """Retenciones de IVA para Venezuela v18.""",
+        'author': 'Ing. Darrell Sojo / Silicon Valley',
+        'summary': 'Retenciones de IVA para Venezuela v19',
+        'description': """Retenciones de IVA para Venezuela v19.""",
         'category': 'Accounting/Accounting',
         'website': '',
         'images': [],
@@ -18,11 +18,11 @@
             ],
         'data': [
             'security/ir.model.access.csv',
-            'views/retention_vat_provee_views.xml',
-            'views/retention_vat_cliente_views.xml',
             'views/partner_views.xml',
             'views/res_company_inherit.xml',
             'views/account_move_views.xml',
+            'views/retention_vat_provee_views.xml',
+            'views/retention_vat_cliente_views.xml',
             'report/vat_wh_voucher.xml',            
             ],
         'installable': True,
