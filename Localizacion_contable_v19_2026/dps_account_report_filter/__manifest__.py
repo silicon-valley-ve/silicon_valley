e35a853e48fa893b4e0d321302fa59d18f020dee
dsojo1980 <dsojo.tanfe@gmail.com>
2026-07-14 00:20:48 -0400

    modulo reporte financiero en usd

--- Localizacion_contable_v19_2026/dps_account_report_filter/__manifest__.py
+++ Localizacion_contable_v19_2026/dps_account_report_filter/__manifest__.py
@@ -1,56 +1,30 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Odoo Financial Reports: Currency Filter | Account Report in Multiple Currencies | Account Reports Multi Currency (Original)",
-    "summary": "Add multi-currency support to financial reports for accurate global financial analysis. Simplify your financial reporting across multiple currencies with our Multi-Currency Accounting Report module. Tailored for businesses handling international transactions, this app delivers accurate, real-time insights into your accounting data across various currencies.",
-    "version": "18.0.6.5.4",
-    "category": "Accounting",
-    'author': 'Dotsprime System',
-    'sequence': 1,
-    'email': 'dotsprime@gmail.com',
-    'support': 'sales@dotsprime.com',
-    "website":'https://dotsprime.com/',
-    #"license": 'OPL-1',
-    'price': 11.75,
-    'currency': 'USD',
-    "description": """
-        Empower your accounting team with enhanced financial reporting by enabling multiple currency support 
-        across key financial statements such as Balance Sheet, Profit and Loss, Cash Flow Statement, Executive Summary, Tax Return, General Ledger, Trail Balance, Journal Audit, Check Register, Partner Ledger, Aged Receivable, Aged Payable.
+    'name': "Odoo Financial Reports: Currency Filter | Account Report in Multiple Currencies | Account Reports Multi Currency (Original)  19",
 
-        This module integrates seamlessly with Odoo's accounting reports to allow switching between the 
-        company's base currency and a selected alternate currency, improving visibility for international 
-        operations and financial audits.
+    'summary': """Odoo Financial Reports: Currency Filter | Account Report in Multiple Currencies | Account Reports Multi Currency (Original)  19""",
 
-        Odoo Financial Reports: Currency Filter :
-
-            Balance Sheet
-            Profit and Loss
-            Cash Flow Statement
-            Executive Summary
-            Tax Return
-            General Ledger
-            Trail Balance
-            Journal Audit
-            Check Register
-            Partner Ledger
-            Aged Receivable
-            Aged Payable
-
-        Key Features:
-        - Add currency selection filters to financial reports
-        - View reports in alternate currencies for better insights
-        - Improve decision-making for businesses with multi-currency transactions
+    'description': """
+       Odoo Financial Reports: Currency Filter | Account Report in Multiple Currencies | Account Reports Multi Currency (Original)  19
+       Colaborador: Ing. Darrell Sojo
     """,
-    "depends": [
-        "account_reports",
-    ],
-    "data": [],
-    "assets": {
-        "web.assets_backend": [
-            "dps_account_report_filter/static/src/components/**/*",
-        ],
-    },
-    'images': ['static/description/main_screenshot.png'],
-    "live_test_url" : "https://youtu.be/2CzQSQYa33g",
-    'demo': [],
-    'application': True,
-}
+    'version': '18.0',
+    'author': 'Ing.Darrell Sojo',
+    'category': 'Módulo base localizacion contable  V19',
 
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'stock',
+        'account',
+        'account_accountant',
+        'account_debit_note',
+        ],
+
+    # always loaded
+    'data': [
+        
+    ],
+    'application': True,
+    'license': 'OEEL-1',
+}
