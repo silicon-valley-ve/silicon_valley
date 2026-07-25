c716950832d4ebb9821b4eb0d96e8664c69c40de
dsojo1980 <dsojo.tanfe@gmail.com>
2026-07-25 00:43:43 -0400

    mod fact digital unidigital

--- Facturacion Digital/factura_digital_unidigital/models/company_inherit.py
+++ Facturacion Digital/factura_digital_unidigital/models/company_inherit.py
@@ -36,6 +36,7 @@ class ResCompany(models.Model):
     enpoint_emision = fields.Char(string="Endpoint Emisión", copy=False, default='/documents/createandapprove')
     enpoint_ultimo_doc = fields.Char(string="Endpoint Último Documento", copy=False)
     seriestrongid = fields.Char(string="SerieStrOngid",copy=False,readonly=True) 
+    sucursal_strong_id = fields.Char(string="SucursalStrongId",copy=False)
 
 
     def unidg_get_token(self):
