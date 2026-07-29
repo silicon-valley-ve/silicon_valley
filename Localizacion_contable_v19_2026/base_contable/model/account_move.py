42b5ab0b7bc3d0db246d9b1300ceed5c9f9bdf33
dsojo1980 <dsojo.tanfe@gmail.com>
2026-07-29 02:35:46 -0400

    ajjuste a nota de credito

--- Localizacion_contable_v19_2026/base_contable/model/account_move.py
+++ Localizacion_contable_v19_2026/base_contable/model/account_move.py
@@ -729,7 +729,7 @@ class  AccountMoveLine(models.Model):
                     # 3. Validar Cantidad
                     original_qty = original_line.quantity
                     credit_note_qty = line.quantity
-                    raise UserError(_("original_qty %s, credit_note_qty %s")%(original_line.product_id.name,line.product_id.name))
+                    raise UserError(_("original_qty %s, credit_note_qty %s")%(original_line.price_unit_ref,line.price_unit_ref))
                     
                     if credit_note_qty > original_qty:
                         raise ValidationError(_(
