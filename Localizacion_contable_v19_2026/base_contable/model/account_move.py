b306cf0d503ec065c76431354813d9b1a76bbdfc
dsojo1980 <dsojo.tanfe@gmail.com>
2026-07-29 04:19:51 -0400

    ajjuste a nota de credito

--- Localizacion_contable_v19_2026/base_contable/model/account_move.py
+++ Localizacion_contable_v19_2026/base_contable/model/account_move.py
@@ -680,14 +680,13 @@ class  AccountMoveLine(models.Model):
     linea_exenta = fields.Boolean(default=False)
 
 
-    @api.onchange('quantity', 'price_unit')
+    @api.constrains('quantity', 'price_unit')
     def _check_credit_note_limits_seniat(self):
         """
         Valida que la cantidad y el precio unitario de la línea de la Nota de Crédito
         no superen los valores de la factura referenciada en el campo 'fact_afect' (número de factura).
         """
         
-        raise UserError(_("nro=%s")%self)
         for line in self:
             move = line.move_id
             
