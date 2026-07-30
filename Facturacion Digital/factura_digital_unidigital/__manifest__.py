7f615727395a03b14d290be029bd6b054016c204
dsojo1980 <dsojo.tanfe@gmail.com>
2026-07-29 20:19:01 -0400

    retenciones iva

--- Facturacion Digital/factura_digital_unidigital/__manifest__.py
+++ Facturacion Digital/factura_digital_unidigital/__manifest__.py
@@ -13,6 +13,7 @@
                 'views/company_views.xml',
                 'views/account_move_views.xml',
                 'views/retention_vat_provee_views.xml',
+                'views/retention_vat_islr_views.xml',
                 
         ],
         'license': 'LGPL-3',
--- Facturacion Digital/factura_digital_unidigital/models/__init__.py
+++ Facturacion Digital/factura_digital_unidigital/models/__init__.py
@@ -3,3 +3,4 @@
 from . import company_inherit
 from . import account_move
 from . import retention_vat
+from . import retention_vat_islr
